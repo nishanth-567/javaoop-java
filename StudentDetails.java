e06043886182c040a988b3190aadfcd7d41b30a9
nishanth-567 <mandavanishanth@gmail.com>
2025-02-28 01:03:13 +0530

    Add files via upload

---
+++ StudentDetails.java
@@ -0,0 +1,14 @@
+public class StudentDetails {
+    public static void main(String[] args) {
+        String name = "Nishanth";
+        int age = 18;
+        String course = "Computer Science and Engineering";
+        String section = "CSE-C";
+
+        System.out.println("Student Details:");
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Course: " + course);
+        System.out.println("Section: " + section);
+    }
+}
